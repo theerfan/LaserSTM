afc12067724531ed04c8aaab2bb3aa56000136fe
Erfan Abedi <erfanabedi.78@gmail.com>
2023-08-02 18:51:39 -0700

    Refactoring analysis (reIm)

--- Analysis/util.py
+++ Analysis/util.py
@@ -1,10 +1,11 @@
-
 import numpy as np
 from scipy.interpolate import interp1d
+from scipy import signal
 
 
 def get_phase(field):
     return np.arctan2(np.imag(field), np.real(field))
+
 
 def resample_method1(input_domain, target_domain, input_vector):
     try:
@@ -15,6 +16,7 @@ def resample_method1(input_domain, target_domain, input_vector):
         print(
             "Likely the target wavelength vector is outside the bounds of the input vector (only interpolation\n"
         )
+
 
 def fft(field):
     """fft with shift
@@ -61,6 +63,66 @@ def energy_match_expanded(field, energy, domain_spacing, spot_area):
     norm_E = np.sum(get_intensity(field)) * domain_spacing * spot_area
     return np.sqrt(energy / norm_E) * field
 
+
+def re_im_sep(fields):
+    shg1 = fields[0:1892] + fields[1892 * 2 + 348 : 1892 * 3 + 348] * 1j
+    shg2 = fields[1892 : 1892 * 2] + fields[1892 * 3 + 348 : 1892 * 4 + 348] * 1j
+    sfg = (
+        fields[1892 * 2 : 1892 * 2 + 348]
+        + fields[1892 * 4 + 348 : 1892 * 4 + 2 * 348] * 1j
+    )
+
+    return shg1, shg2, sfg
+
+
+def change_domains(domain, field, new_domain, domain_type):
+    padded_vector = np.pad(field, (1, 1), mode="constant")
+    window = signal.windows.tukey(int(len(padded_vector) // 1))
+    padded_vector = window * padded_vector
+
+    # Extend old domain to match new domain
+    alt_domain = np.append([new_domain[0]], domain)
+    alt_domain = np.append(alt_domain, [new_domain[-1]])
+
+    # Resample padded vector using new domain
+    resampled_vector = resample_method1(alt_domain, new_domain, padded_vector)
+
+    if domain_type == "freq":
+        out_direct = ifft(field)
+        out = ifft(resampled_vector)
+    elif domain_type == "time":
+        out_direct = fft(field)
+        out = fft(resampled_vector)
+    else:
+        print("field type not supported")
+
+    return out_direct, out
+
+
+def change_domain_and_adjust_energy(
+    domain,
+    field,
+    new_domain,
+    domain_type,
+    beam_area,
+    domain_spacing,
+    true_domain_spacing,
+):
+    out_direct, out = change_domains(domain, field, new_domain, domain_type)
+    domain_spacing_calc = domain[1] - domain[0]
+    pulse_energy_calc = calc_energy_expanded(field, domain_spacing_calc, beam_area)
+    out_direct = energy_match_expanded(
+        out_direct, pulse_energy_calc, domain_spacing, beam_area
+    )
+    out = energy_match_expanded(out, pulse_energy_calc, true_domain_spacing, beam_area)
+
+    print(
+        "direct, and resampled energies: ",
+        calc_energy_expanded(out_direct, domain_spacing, beam_area),
+        calc_energy_expanded(out, true_domain_spacing, beam_area),
+    )
+
+    return out_direct, out
 
 
 class UNITS:
