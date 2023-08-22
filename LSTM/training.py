172a930df0381046491285f68093c412818fabd4
Erfan Abedi <erfanabedi.78@gmail.com>
2023-08-21 17:32:49 -0700

    Minor refactoring to make code shape consistent

--- LSTM/training.py
+++ LSTM/training.py
@@ -12,45 +12,7 @@ from train_predict_utils import (
     weighted_MSE,
 )
 
-
-class LSTMModel_1(nn.Module):
-    # basic one with two linear layers and final output with sigmoid
-    def __init__(
-        self, input_size, lstm_hidden_size=1024, linear_layer_size=4096, num_layers=1
-    ):
-        super().__init__()
-        self.input_size = input_size
-        self.num_layers = num_layers
-        self.hidden_size = lstm_hidden_size
-        self.lstm = nn.LSTM(
-            input_size,
-            lstm_hidden_size,
-            batch_first=True,
-            dropout=0,
-            num_layers=num_layers,
-        )
-        self.fc1 = nn.Linear(lstm_hidden_size, linear_layer_size)
-        self.fc2 = nn.Linear(linear_layer_size, linear_layer_size)
-        self.fc3 = nn.Linear(linear_layer_size, input_size)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        # hidden state
-        h_0 = torch.zeros(self.num_layers * 1, x.size(0), self.hidden_size).to(
-            x.device
-        )  # Modified line
-        # cell state
-        c_0 = torch.zeros(self.num_layers * 1, x.size(0), self.hidden_size).to(
-            x.device
-        )  # Modified line
-
-        out, (hn, cn) = self.lstm(x, (h_0, c_0))
-        out = self.fc1(out[:, -1, :])
-        out = self.relu(out)
-        out = self.fc3(out)
-        out = torch.sigmoid(out)
-
-        return out
+from LSTM.model import LSTMModel_1
 
 
 def dev_test_losses():
@@ -146,9 +108,7 @@ if __name__ == "__main__":
         "--epoch_save_interval", type=int, default=1, help="Epoch save interval."
     )
 
-    parser.add_argument(
-        "--output_dir", type=str, default=".", help="Output directory."
-    )
+    parser.add_argument("--output_dir", type=str, default=".", help="Output directory.")
 
     loss_dict = {
         "weighted_MSE": weighted_MSE,
