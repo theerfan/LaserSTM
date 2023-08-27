825afac970a98e89b1a6f06cece4b5847b19fd92
Erfan Abedi <erfanabedi.78@gmail.com>
2023-08-27 14:02:22 -0700

    rename bash scripts

--- config.sh
+++ config.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Step 1: get the IP.
+# Step 1: Load the modules
+module load cuda/11.8 
+module load python/3.9.6
+pip3 install -r requirements.txt
+
+# Step 2: get the IP.
 IP=$(hostname -I | awk '{print $1}')
 
 # Step 2: Replace the IP in launch.json with the new IP.
--- ix_load.sh
+++
@@ -1,3 +0,0 @@
-module load cuda/11.8 
-module load python/3.9.6
-pip3 install -r requirements.txt
