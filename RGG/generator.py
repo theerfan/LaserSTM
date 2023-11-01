0f5a7cfc195592264be3288857773ad1ada0a762
Erfan Abedi <erfanabedi.78@gmail.com>
2023-10-31 20:58:05 -0700

    improvements on the generator

--- RGG/generator.py
+++ RGG/generator.py
@@ -32,14 +32,32 @@ def generate_gaussian(
 
 # Visualize the gaussian distribution
 def visualize_gaussian(gaussian: np.ndarray):
-    plt.hist(gaussian, bins=200)
+    # plt.hist(, bins=200, label="w", color="blue")
+    # plt.hist(, bins=200, label="E", color="green")
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Create the vertical histogram on the first axes
+    axs[0].hist(gaussian["frequencies"], bins=200, alpha=0.5, label='Freq (V)', color='blue')
+    axs[0].set_xlabel('Value')
+    axs[0].set_ylabel('Occurance')
+    axs[0].legend(loc='upper right')
+
+    # Create the horizontal histogram on the second axes
+    axs[1].hist(gaussian["phases"], bins=200, alpha=0.5, label='Phases (H)', color='green', orientation='horizontal')
+    axs[1].set_xlabel('Occurance')
+    axs[1].set_ylabel('Value')
+    axs[1].legend(loc='upper right')
+
+    axs[1].text(0.95, 0.0, f'Energy = {gaussian["energy"]}', horizontalalignment='right',
+         verticalalignment='bottom', transform=plt.gca().transAxes)
     plt.show()
+    plt.savefig("gaussian.png")
 
 
 # Fix the minimum value of the gaussian distribution to 0
 def fix_min(gaussian: np.ndarray):
-    if min(gaussian) < 0:
-        gaussian -= min(gaussian)
+    if mini := np.min(gaussian) < 0:
+        gaussian -= mini
     else:
         pass
     return gaussian
@@ -73,7 +91,7 @@ def generate_gaussian_combination(
         total_gaussian["energy"] += sign * current_gaussian["energy"]
 
     total_gaussian["frequencies"] = fix_min(total_gaussian["frequencies"])
-    total_gaussian["energies"] = fix_min(total_gaussian["energies"])
+    total_gaussian["energy"] = fix_min(total_gaussian["energy"])
 
     total_gaussian["phases"] = np.array(
         [
@@ -85,5 +103,5 @@ def generate_gaussian_combination(
     return total_gaussian
 
 
-a = generate_gaussian_combination(10000, [0, 0], [1, 1], [1, 1])
+a = generate_gaussian_combination(10000, means=[0.2, 0.1], stds=[2, 1], energies=[0.1, 1], init_phases=[0.1, 0.2])
 visualize_gaussian(a)
